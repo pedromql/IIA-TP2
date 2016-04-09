187d0dc20396b9112b10f7b8bb085464ffd684d8
pedromql <pedromql@gmail.com>
2016-04-09 17:36:48 +0100

    Initial support scripts

---
+++ SearchProblem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public struct Successor
+{
+	public object state;
+	public float cost;
+	public Action action;
+
+
+	public Successor(object state, float cost, Action a)
+	{
+		this.state = state;
+		this.cost = cost;
+		this.action = a;
+	}
+}
+
+
+public interface ISearchProblem
+{
+	object GetStartState ();
+	bool IsGoal (object state);
+	Successor[] GetSuccessors (object state);
+
+	int GetVisited ();
+	int GetExpanded ();
+}
